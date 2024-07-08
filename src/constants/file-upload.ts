f63402dabbef1bcf912e3e97626cc9aa8625b087
João Conserva <boscoj@outlook.com>
2024-07-08 17:29:41 -0300

    chore: Update maximum file upload size to 15 MB and add file size price constant

--- src/constants/file-upload.ts
+++ src/constants/file-upload.ts
@@ -1,4 +1,3 @@
-export const MAX_FILE_UPLOAD_SIZE = 30 * 1024 * 1024;  // 30 MB
-
-
+export const MAX_FILE_UPLOAD_SIZE = 15 * 1024 * 1024; // 15 MB
+export const FILE_SIZE_PRICE = 0.1;
 export const MAX_FILE_USER_STORAGE = 2147483648 / 2;
